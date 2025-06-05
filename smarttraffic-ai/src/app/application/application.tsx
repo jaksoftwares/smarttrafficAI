c2314369d288653c43be98fbc144f65902de70ce
jaksoftwares <jaksoftwares05@gmail.com>
2025-06-05 10:51:44 +0300

    commits

--- smarttraffic-ai/src/app/application/application.tsx
+++ smarttraffic-ai/src/app/application/application.tsx
@@ -1,5 +1,5 @@
-import HomePage from "./home/page";
+// import HomePage from "./home/page";
 
-export default function Page() {
-  return <HomePage />;
-}
+// export default function Page() {
+//   return <HomePage />;
+// }
